9fb0c04adfaef34cd2c4f64bd7b2c926d9b33ea3
avaldez84 <52429643+avaldez84@users.noreply.github.com>
2019-07-09 15:13:31 -0300

    Unidad1
    Resuelta

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,27 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    //declaramos las variables a usar
+    var suma;
+    var Uno;
+    var Dos;
+
+    Uno = document.getElementById("numeroUno").value;  //sacamos los valores del ID
+    Dos = document.getElementById("numeroDos").value;
+
+    Uno=parseInt(Uno) //convertimos las cadenas de texto a numericas
+    Dos=parseInt(Dos)
+
+    //sumamos las dos variables ya pasadas a numericas
+
+    suma = Uno+Dos;
+
+    //mostramos es resultado de "suma" por alert mas la cadena de texto "la suma es"
+
+    alert("La suma es " + suma);
+    
 
 }
+
+
 
